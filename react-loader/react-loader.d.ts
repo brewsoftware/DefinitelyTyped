3de0ef52a8771c2eb23376fbcff5b8e072fd1351
Jason Turner <jturner@liberty.com.au>
2015-12-22 15:14:17 +1100

    new react loader definition

---
+++ react-loader/react-loader.d.ts
@@ -0,0 +1,37 @@
+﻿///<reference path="./react/react.d.ts"/>
+declare module 'react-loader' {
+    import React = require("react");
+
+    // Form component
+    var Loader: LoaderClass;
+
+    interface Loader extends React.ReactElement<LoaderProps> {
+    }
+
+    interface LoaderClass extends React.ComponentClass<LoaderProps> {
+    }
+
+    interface LoaderProps extends React.Props<LoaderClass> {
+
+        loaded: boolean;
+        lines?: number;
+        length?: number;
+        width?: number;
+        radius?: number;
+        
+        corners?: number;
+        rotate?: number;
+        direction?: number;
+        color?: string;
+        speed?: number;
+        trail?: number;
+        shadow?: boolean;
+        hwaccel?: boolean;
+        className?: string;
+        zIndex?: number;
+        top?: string;
+        left?: string;
+        scale?: number;
+        options?: any;   
+    }
+}
