0aa2e2c2e6181290eea5de0ac644a4e13c32acf9
Jason Turner <jturner@liberty.com.au>
2016-04-06 11:35:09 +1000

    Added react bootstrap component type definitions

---
+++ react-dropzone-component/react-dropzone-component.d.ts
@@ -0,0 +1,47 @@
+// Type definitions for react-dropzone-component
+// Project: https://github.com/felixrieseberg/React-Dropzone-Component
+// Definitions by: Jason Turner (Brew Software) <https://github.com/brewsoftware>
+// Definitions: https://github.com/brewsoftware/DefinitelyTyped
+
+///<reference path='../react/react.d.ts' />
+
+declare namespace ReactDropzoneComponent {
+    import React = __React;
+    interface DropzoneProps {
+        onDrop?: Function;
+        onDropAccepted?: Function;
+        onDropRejected?: Function;
+        onDragEnter?: Function;
+        onDragLeave?: Function;
+        style?: Object;
+        activeStyle?: Object;
+        className?: string;
+        activeClassName?: string;
+        rejectClassName?: string;
+        /**
+        * Clicking the <Dropzone> brings up the browser file picker. To disable, set to true.
+        */
+        disableClick?: boolean;
+        /**
+        * To accept only a single file, set this to false.
+        */
+        multiple?: boolean;
+        /**
+        * Filters the file types that are valid. It should have a valid MIME type according to input element, for example:
+        * application/pdf
+        * image/*
+        * audio/aiff,audio/midi
+        */
+        accept?: string;
+    }
+
+    export class Dropzone extends React.Component<DropzoneProps, {}> {
+    }
+
+}
+
+declare module "react-dropzone-component" {
+    const Dropzone: typeof ReactDropzoneComponent.Dropzone;
+    export = Dropzone;
+}
+
